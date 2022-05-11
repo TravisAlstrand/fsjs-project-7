0eb79b5a98c72b5ae7c4868e4d49f5e1c57ee4e3
TravisAlstrand <travisalstrand@gmail.com>
2022-05-11 09:41:06 -0700

    Commit 7
    Basic Search-Form class created and rendering.

--- react-gallery-app/src/components/App.js
+++ react-gallery-app/src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 // components
+import SearchForm from './SearchForm';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
 
 function App() {
   return (
     <div className='container'>
+
+      <SearchForm />
 
       <Nav />
 
